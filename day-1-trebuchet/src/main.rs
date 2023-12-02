429050e6c44d618e7006db48d9cdaa7f2af6a7b9
tuzymandias <>
2023-12-03 08:30:32 +1100

    [aoc-2023]: day 1 part 2 completed

--- day-1-trebuchet/src/main.rs
+++ day-1-trebuchet/src/main.rs
@@ -1,11 +1,34 @@
+use std::cmp;
+
 const INPUT: &'static str = include_str!("input.txt");
-const RADIX: u32 = 10;
+
+// Modified from: https://users.rust-lang.org/t/iterator-over-windows-of-chars/17841/2
+// Iterate through substrings of a string with window size of n
+// Window gets smaller as it approaches the end
+fn str_windows(line: &str, n: usize) -> impl Iterator<Item = &str> {
+    line.char_indices()
+        .map(move |(i, _)| &line[i..cmp::min(i+n, line.len())])
+}
+
+fn parse(s: &str) -> Option<i32> {
+    match s {
+        _ if s.starts_with("one") | s.starts_with('1') => Some(1),
+        _ if s.starts_with("two") | s.starts_with('2') => Some(2),
+        _ if s.starts_with("three") | s.starts_with('3') => Some(3),
+        _ if s.starts_with("four") | s.starts_with('4') => Some(4),
+        _ if s.starts_with("five") | s.starts_with('5') => Some(5),
+        _ if s.starts_with("six") | s.starts_with('6') => Some(6),
+        _ if s.starts_with("seven") | s.starts_with('7') => Some(7),
+        _ if s.starts_with("eight") | s.starts_with('8') => Some(8),
+        _ if s.starts_with("nine") | s.starts_with('9') => Some(9),
+        _ => None
+    }
+}
 
 fn compute_calibration_value(line: &str) -> i32 {
-    let (first, last) = line.chars()
-        .filter(|c| c.is_numeric())
-        .map(|c| c.to_digit(RADIX).unwrap() as i32)
-        .fold((None, None), |(first, last), number | {
+    let pair = str_windows(line, 5)
+        .filter_map(|window| parse(window))
+        .fold((None, None), |(first, last), number| {
             match (first, last) {
                 (None, None) => (Some(number), Some(number)),
                 (Some(a), _) => (Some(a), Some(number)),
@@ -13,9 +36,10 @@ fn compute_calibration_value(line: &str) -> i32 {
             }
         });
 
-    let (first, last) = (first.unwrap_or(0), last.unwrap_or(0));
-
-    first * 10 + last
+    match pair {
+        (Some(first), Some(last)) => first * 10 + last,
+        _ => 0
+    }
 }
 
 fn main() {
@@ -23,5 +47,5 @@ fn main() {
         .map(|line| compute_calibration_value(line))
         .fold(0, |sum, cv| sum + cv);
 
-    println!("{}", sum_cv)
+    println!("{}", sum_cv);
 }
